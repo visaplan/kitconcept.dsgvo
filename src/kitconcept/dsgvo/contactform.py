13a7d778afff1c6c3f81b90ece7f78e3d67dd6b7
Carsten Senger <senger@rehfisch.de>
2018-05-18 18:51:18 +0200

    Extend contact form to show privacy info

---
+++ src/kitconcept/dsgvo/contactform.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.browser.contact_info import ContactForm
+
+from plone.supermodel import model
+from plone.z3cform.fieldsets import extensible
+
+from z3c.form import field
+
+from zope import schema
+from zope.component import adapts
+from zope.interface import Interface
+
+from kitconcept.dsgvo import _
+from kitconcept.dsgvo.interfaces import IKitconceptDsgvoLayer
+from kitconcept.dsgvo.widget import DsgvoSingleCheckBoxBoolFieldWidget
+
+
+class IDsgvoContactInfoSchema(model.Schema):
+
+    dsgvo_contact_info_text = schema.Bool(
+        title=_(
+            u'label_dsgvo_info',
+            default=(
+                u'Ihre Anfrage wird verschlüsselt per https an unseren '
+                u'Server geschickt. Sie erklären sich damit einverstanden, '
+                u'dass wir die Angaben zur Beantwortung Ihrer Anfrage '
+                u'verwenden dürfen. Hier finden Sie unsere '
+                u'<a href="${portal_url}/privacy">Datenschutzerklärung und '
+                u'Widerrufhinweise</a>.')),
+        description=_(
+            u'help_dsgvo_info',
+            default=u''),
+        default=True,
+    )
+
+
+class ContactFormExtender(extensible.FormExtender):
+    adapts(Interface, IKitconceptDsgvoLayer, ContactForm)
+
+    def update(self):
+        fields = field.Fields(IDsgvoContactInfoSchema)
+        fields['dsgvo_contact_info_text'].widgetFactory = \
+            DsgvoSingleCheckBoxBoolFieldWidget
+        self.add(fields)
