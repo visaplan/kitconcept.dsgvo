f36adb305f5c2065b4b9c82755c2ee91d50355b6
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2020-04-27 04:30:24 -0300

    Add export users view

---
+++ src/kitconcept/dsgvo/browser/users.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+from datetime import datetime
+from io import StringIO
+from plone import api
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+
+import csv
+
+
+class ExportUsers(BrowserView):
+    '''Export the users'''
+
+    def __call__(self):
+        mtool = getToolByName(self.context, 'portal_membership')
+        members = mtool.listMembers()
+        headers = ['Name', 'Email']
+        data = []
+        for member in members:
+            info = {
+                'Name': member.getProperty('fullname', ''),
+                'Email': member.getProperty('email', ''),
+            }
+            data.append(info)
+
+        out = StringIO()
+        writer = csv.DictWriter(out, headers)
+        writer.writerow(dict(zip(headers, headers)))
+        writer.writerows(data)
+
+        now = datetime.now().strftime('%Y-%m-%d-%H_%M_%s')
+        filename = 'ehrenamtsportal-benutzerexport-%s.csv' % now
+        self.request.response.setHeader(
+            'Content-Disposition', 'attachment; Filename=%s' % filename)
+        self.request.response.setHeader("Content-type", "text/csv")
+        return out.getvalue()
